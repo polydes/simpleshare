7c00fd881f8984554b48f81b10c06abfdf2f16c7
Svintaj <Svintaj@users.noreply.github.com>
2023-02-23 23:20:35 +0900

    Changes received from svintaj to fix sharing images on Android 5.0+

--- dependencies/simpleshare/src/com/byrobin/simpleshare/Share.java
+++ dependencies/simpleshare/src/com/byrobin/simpleshare/Share.java
@@ -36,6 +36,11 @@ import android.util.Log;
 
 import org.haxe.extension.Extension;
 
+import androidx.core.content.FileProvider;
+import java.util.Objects;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 ///permisions
 import android.content.pm.PackageManager;
 import android.Manifest;
@@ -56,12 +61,12 @@ public class Share extends Extension
     private static boolean shareFailed = false;
 
     public static void shareContent(final String msg, final String url, final boolean withImage)
-	{
+    {
         
         mainActivity.runOnUiThread(new Runnable()
         {
-        	public void run()
-			{
+            public void run()
+            {
                 
                 if(!withImage)
                 {
@@ -69,8 +74,8 @@ public class Share extends Extension
                     Intent intent = new Intent(android.content.Intent.ACTION_SEND);
                     intent.setType("text/plain");
                     intent.putExtra(Intent.EXTRA_TEXT, msg + "\n\n" + url);
-                    Extension.mainContext.startActivity(Intent.createChooser(intent, "Share via.."));
-                    
+                    Extension.mainContext.startActivity(intent);
+
                     shareSucceed = true;
                     shareFailed = false;
                     
@@ -94,31 +99,78 @@ public class Share extends Extension
                 Bitmap image = convertToImage(base64Img);
                 File filePath = null;
                 
+                // Create unique filenames for each share (Good when sharing to a directory. -> If the file already exists -> and you don't have the option to rename the file)
+                SimpleDateFormat dateFormat = new SimpleDateFormat("_yyyy-MM-dd_HH.mm.ss");
+                String currentDateTime = dateFormat.format(new Date());
+
                 try {
                     StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
                     StrictMode.setVmPolicy(builder.build());
 
-                    filePath = new File(Extension.mainContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "screen.png");
+                    filePath = new File(Extension.mainContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "Screen" +currentDateTime +".png"); // Use .png or .jpg
                     FileOutputStream fos = new FileOutputStream(filePath);
 
-                    image.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                    image.compress(Bitmap.CompressFormat.PNG, 100, fos); // Use PNG or JPEG
                     fos.close();
                     
                 } catch (Exception e) {
                     Log.e("saveToInternalStorage()", e.getMessage());
                 }
+                        
+                Log.v("Simpleshare file path: ", "filePath is: "+filePath.toString());
                 
-                //File filePath = Extension.mainContext.getFileStreamPath("screen.png");
+                // Fixing the : FileUriExposedViolation error:                                                                  
+                // Using a FileProvider (Other apps can then read the image-file temporarly when we share it)           
+                Uri fileUri = FileProvider.getUriForFile( Objects.requireNonNull( Extension.mainContext.getApplicationContext() ), "com.game.name" +".provider", filePath);  // "com.game.name.provider" <- Justin! This ID, MUST be worldwide UNIQUE, and should ideally be generated automatically at build time, it should then be set to the BundleID of the curent Stencyl Game +".provider" -> The same ID should also dynamically be created in the Game Android Manifest at build time (I used the template in workspace)
                 
-                Log.e("file path ", filePath.toString());
-                
-                Uri fileUri = Uri.fromFile(filePath);
-                
-                Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+                        // To get FileProvider to work together with Stencyl:
+                        // I have modified the template manifest in [Stecyl] \plaf\lime-templates\android\template\app\src\main 
+                        // , to be able to set up the <provider>-tags (Sice they hade to be placed between the <application>-tags).
+                        //
+                        // Ideally -> this should be automated so that you replace this static string: "com.game.name.provider" with the
+                        // games BundleID +".provider", both in the Android Manifest for the game as well as in -> this Share.java code.
+
+                            /*
+                                <application> 
+                                .....
+
+                                    <!-- This is added to the template manifest: -->>
+
+                                    <provider
+                                        android:name="androidx.core.content.FileProvider" 
+                                        android:authorities="com.game.name.provider"   
+                                        android:exported="false"
+                                        android:grantUriPermissions="true">
+                                        <meta-data
+                                            android:name="android.support.FILE_PROVIDER_PATHS"
+                                            android:resource="@xml/file_paths">
+                                        </meta-data>
+                                    </provider>         
+
+
+
+                                .....                       
+                                </application>
+
+                            */ 
+                    
+                        //  AND -> I have ALSO in [Stecyl] \plaf\lime-templates\android\template\app\src\main -> under [res] -> created a new folder: [xml] AND added the file: file_paths.xml  
+                        
+                            /*
+                                    The file: file_paths.xml contains: 
+                                
+                                    <?xml version="1.0" encoding="utf-8"?> 
+                                    <paths xmlns:android="http://schemas.android.com/apk/res/android"> 
+                                                <external-path name="external_files" path="." /> 
+                                    </paths>
+                            */
+                    
+                Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("image/*");
                 intent.putExtra(Intent.EXTRA_TEXT, msg + "\n\n" + url);
                 intent.putExtra(Intent.EXTRA_STREAM, fileUri);
-                Extension.mainContext.startActivity(Intent.createChooser(intent, "Share via.."));
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // If write permission needed use: | Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                Extension.mainContext.startActivity(intent);     
                 
                 shareSucceed = true;
                 shareFailed = false;
